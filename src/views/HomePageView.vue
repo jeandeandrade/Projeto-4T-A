24ba95f15e9d6cba3e9492375a6077fff6c81d8b
Malagadixx <127902976+Malagadixx@users.noreply.github.com>
2024-09-03 13:05:54 -0300

    create testimonials

--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -1,11 +1,13 @@
 <script setup>
-import Hero from '../views/HeroView.vue';
-import NavBar from '../views/NavBarView.vue'
+import Hero from "../views/HeroView.vue";
+import NavBar from "../views/NavBarView.vue";
+import BoxSwipe from "../views/BoxSwipeView.vue";
 </script>
 
 <template>
   <main>
     <NavBar />
     <Hero />
+    <BoxSwipe />
   </main>
 </template>
