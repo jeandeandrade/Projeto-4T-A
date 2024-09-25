911dd9a9ecd65bd18471269618fd9770c89f102c
edueevaristo <eduevaristo_dev@hotmail.com>
2024-09-25 02:43:49 -0300

    [feature/sm-modificacoes-novos-produtos] - Commit Feature | Ajustando tamanhos

--- src/components/NewProducts.vue
+++ src/components/NewProducts.vue
@@ -1,45 +1,27 @@
 <script setup></script>
 
 <template>
-  <main
-    class="main__content max-w-[1400px] h-auto ml-[16px] lg:ml-[119px] shrink-0 bg-[#F3F5F9] rounded-[30px]"
-  >
-    <section
-      class="content__products flex flex-col items-center gap-[40px] lg:gap-[80px] p-4 lg:p-0"
-    >
-      <div
-        class="title__content flex flex-col items-center gap-[8px] lg:gap-[16px]"
-      >
+  <main class="main__content h-auto ml-[16px] lg:ml-[119px] shrink-0 bg-[#F3F5F9] rounded-[30px]">
+    <section class="content__products flex flex-col items-center gap-[40px] lg:gap-[80px] p-4 lg:p-0">
+      <div class="title__content flex flex-col items-center gap-[8px] lg:gap-[16px]">
         <h1
-          class="text-black font-[Inter] text-[24px] lg:text-[36px] not-italic font-semibold leading-[32px] lg:leading-[44px] tracking-[-0.72px] lg:tracking-[-1.44px]"
-        >
+          class="text-black font-[Inter] text-[24px] lg:text-[36px] not-italic font-semibold leading-[32px] lg:leading-[44px] tracking-[-0.72px] lg:tracking-[-1.44px]">
           NEW PRODUCTS
         </h1>
         <p
-          class="text-[#8a8a8a] text-center font-[Inter] text-[14px] lg:text-[18px] not-italic font-normal leading-[22px] lg:leading-[28px] tracking-[-0.14px] lg:tracking-[-0.18px]"
-        >
+          class="text-[#8a8a8a] text-center font-[Inter] text-[14px] lg:text-[18px] not-italic font-normal leading-[22px] lg:leading-[28px] tracking-[-0.14px] lg:tracking-[-0.18px]">
           Choose the headset that best suits your needs. We offer top-quality
           options with <br class="hidden lg:block" />
           a variety of features to enhance your audio experience
         </p>
-        <div
-          class="showcase__container flex flex-wrap justify-center lg:justify-around gap-[16px] lg:gap-[32px]"
-        >
+        <div class="showcase__container flex flex-wrap justify-center lg:justify-around gap-[16px] lg:gap-[32px]">
           <!-- Product 1 -->
-          <div
-            class="showcase__products flex flex-col items-center gap-[16px] lg:gap-[32px] w-full max-w-[304px]"
-          >
-            <img
-              class="w-full h-[327px] bg-cover bg-no-repeat"
-              src="../assets/images/headsetProduct.svg"
-              alt="headsets"
-            />
-            <div
-              class="text__products flex flex-col items-center gap-[16px] lg:gap-[24px]"
-            >
+          <div class="showcase__products flex flex-col items-center gap-[16px] lg:gap-[32px] w-full max-w-[304px]">
+            <img class="w-full h-[327px] bg-cover bg-no-repeat" src="../assets/images/headsetProduct.svg"
+              alt="headsets" />
+            <div class="text__products flex flex-col items-center gap-[16px] lg:gap-[24px]">
               <p
-                class="text-[#333] font-[Inter] text-[14px] lg:text-[18px] not-italic font-normal leading-[22px] lg:leading-[28px] tracking-[-0.14px] lg:tracking-[-0.18px] text-center"
-              >
+                class="text-[#333] font-[Inter] text-[14px] lg:text-[18px] not-italic font-normal leading-[22px] lg:leading-[28px] tracking-[-0.14px] lg:tracking-[-0.18px] text-center">
                 Gabini® K-29 Premium Headset with Multi-Device Connectivity and
                 Noise-Canceling Technology, Black.
               </p>
@@ -50,35 +32,20 @@
                   <span>$139.99</span>
                 </h3>
               </div>
-              <button
-                class="button__cart bg-white border-[1px] border-[#B5B5B5] rounded-full pl-4 pr-2 py-3"
-              >
+              <button class="button__cart bg-white border-[1px] border-[#B5B5B5] rounded-full pl-4 pr-2 py-3">
                 ADD TO CART
-                <img
-                  class="ml-4"
-                  src="../assets/icons/cart.svg"
-                  alt=""
-                  style="height: 24px; width: 24px"
-                />
+                <img class="ml-4" src="../assets/icons/cart.svg" alt="" style="height: 24px; width: 24px" />
               </button>
             </div>
           </div>
 
           <!-- Product 2 -->
-          <div
-            class="showcase__products flex flex-col items-center gap-[16px] lg:gap-[32px] w-full max-w-[304px]"
-          >
-            <img
-              class="w-full h-[327px] bg-cover bg-no-repeat"
-              src="../assets/images/headsetProduct.svg"
-              alt="headsets"
-            />
-            <div
-              class="text__products flex flex-col items-center gap-[16px] lg:gap-[24px]"
-            >
+          <div class="showcase__products flex flex-col items-center gap-[16px] lg:gap-[32px] w-full max-w-[304px]">
+            <img class="w-full h-[327px] bg-cover bg-no-repeat" src="../assets/images/headsetProduct.svg"
+              alt="headsets" />
+            <div class="text__products flex flex-col items-center gap-[16px] lg:gap-[24px]">
               <p
-                class="text-[#333] font-[Inter] text-[14px] lg:text-[18px] not-italic font-normal leading-[22px] lg:leading-[28px] tracking-[-0.14px] lg:tracking-[-0.18px] text-center"
-              >
+                class="text-[#333] font-[Inter] text-[14px] lg:text-[18px] not-italic font-normal leading-[22px] lg:leading-[28px] tracking-[-0.14px] lg:tracking-[-0.18px] text-center">
                 Gabini® K-29 Premium Headset with Multi-Device Connectivity and
                 Noise-Canceling Technology, Black.
               </p>
@@ -89,35 +56,20 @@
                   <span>$139.99</span>
                 </h3>
               </div>
-              <button
-                class="button__cart bg-white border-[1px] border-[#B5B5B5] rounded-full pl-4 pr-2 py-3"
-              >
+              <button class="button__cart bg-white border-[1px] border-[#B5B5B5] rounded-full pl-4 pr-2 py-3">
                 ADD TO CART
-                <img
-                  class="ml-4"
-                  src="../assets/icons/cart.svg"
-                  alt=""
-                  style="height: 24px; width: 24px"
-                />
+                <img class="ml-4" src="../assets/icons/cart.svg" alt="" style="height: 24px; width: 24px" />
               </button>
             </div>
           </div>
 
           <!-- Product 3 -->
-          <div
-            class="showcase__products flex flex-col items-center gap-[16px] lg:gap-[32px] w-full max-w-[304px]"
-          >
-            <img
-              class="w-full h-[327px] bg-cover bg-no-repeat"
-              src="../assets/images/headsetProduct.svg"
-              alt="headsets"
-            />
-            <div
-              class="text__products flex flex-col items-center gap-[16px] lg:gap-[24px]"
-            >
+          <div class="showcase__products flex flex-col items-center gap-[16px] lg:gap-[32px] w-full max-w-[304px]">
+            <img class="w-full h-[327px] bg-cover bg-no-repeat" src="../assets/images/headsetProduct.svg"
+              alt="headsets" />
+            <div class="text__products flex flex-col items-center gap-[16px] lg:gap-[24px]">
               <p
-                class="text-[#333] font-[Inter] text-[14px] lg:text-[18px] not-italic font-normal leading-[22px] lg:leading-[28px] tracking-[-0.14px] lg:tracking-[-0.18px] text-center"
-              >
+                class="text-[#333] font-[Inter] text-[14px] lg:text-[18px] not-italic font-normal leading-[22px] lg:leading-[28px] tracking-[-0.14px] lg:tracking-[-0.18px] text-center">
                 Gabini® K-29 Premium Headset with Multi-Device Connectivity and
                 Noise-Canceling Technology, Black.
               </p>
@@ -128,16 +80,9 @@
                   <span>$139.99</span>
                 </h3>
               </div>
-              <button
-                class="button__cart bg-white border-[1px] border-[#B5B5B5] rounded-full pl-4 pr-2 py-3"
-              >
+              <button class="button__cart bg-white border-[1px] border-[#B5B5B5] rounded-full pl-4 pr-2 py-3">
                 ADD TO CART
-                <img
-                  class="ml-4"
-                  src="../assets/icons/cart.svg"
-                  alt=""
-                  style="height: 24px; width: 24px"
-                />
+                <img class="ml-4" src="../assets/icons/cart.svg" alt="" style="height: 24px; width: 24px" />
               </button>
             </div>
           </div>
@@ -151,7 +96,9 @@
 .main__content {
   padding: 50px;
   padding-top: 80px;
-  margin: 10px 100px 50px 100px;
+  margin-bottom: 4%;
+  width: 97%;
+  margin-left: 31px;
 }
 
 .showcase__container {
