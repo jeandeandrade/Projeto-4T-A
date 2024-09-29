6546957098a2111f62fee22ed634418662051af5
Jean Andrade <jeancarlos.av0@gmail.com>
2024-09-29 20:35:56 -0300

    [feature/gift-selection] - Commit Feature | Subindo a parte referente aos presentes, commit inicial

--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -6,12 +6,14 @@ import Offer from '../components/Offer.vue';
 import BoxSwipe from "../views/BoxSwipeView.vue";
 import NewProducts from '@/components/NewProducts.vue';
 import ProductEmphasis from '../views/ProductEmphasisView.vue';
+import GiftSelection from './GiftSelectionView.vue';
 </script>
 
 <template>
   <main>
     <NavBar />
     <Hero />
+    <GiftSelection/>
     <NewProducts/>
     <Offer end-date="2024-12-31T23:59:59" />
     <ProductEmphasis />
