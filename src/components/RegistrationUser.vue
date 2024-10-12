20716473fb2edfda57be99ba38a8f2c6125c5848
DanielVieiraCosta <dvieiradacosta777@gmail.com>
2024-10-11 21:07:13 -0300

    inicio nav bar

--- src/components/RegistrationUser.vue
+++ src/components/RegistrationUser.vue
@@ -4,19 +4,27 @@
 
 <template>
   <main>
-    <div class="registro">
+    <nav class="p-4">
+      <div class="container mx-auto flex justify-between items-center">
+          <a href="#" class="bg-gradient-to-r from-blue-500 to-green-500 text-transparent bg-clip-text hover:text-white">Home</a>
+          <a href="#" class="text-#718EBF hover:text-white">Serviços</a>
+      </div>
+    </nav>
+    <div class="registro grid grid-cols-2">
+      <div>
+        <img src="../assets/images/fotoPerfil.png" alt="">
+      </div>
+      <div>
         <label>E-mail</label>
         <input type="email" id="email" placeholder="Ex: eduardo@gmail.com" class="block"></input>
         <router-link to="/"><button class="button flex items-center"><b>Registrar-se</b></button></router-link>
+      </div>
+
     </div>
   </main>
 </template>
 
 <style scoped>
-.registro{
-    background-color: #101010;
-    color: white;
-}
 .button{
     padding: 10px 30px;
     margin-top: 20px;
@@ -27,5 +35,7 @@
 #email{
     border-radius: 5px;
     color: black;
+    background-color: #F1F3F6;
+    padding: 10px;
 }
 </style>
