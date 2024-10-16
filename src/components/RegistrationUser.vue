327a18ca2fc5ee8bfae5c62528a8e49636d73a45
DanielVieiraCosta <dvieiradacosta777@gmail.com>
2024-10-15 21:26:48 -0300

    add seleção nos campos

--- src/components/RegistrationUser.vue
+++ src/components/RegistrationUser.vue
@@ -12,11 +12,9 @@ background: linear-gradient(90deg, #03B1FF 0%, #97C4D8 100%);
           <b>Dados Pessoais</b>
           <span class="absolute bottom-0 left-0 w-full h-[4px] bg-gradient-to-r from-[#03B1FF] to-[#97C4D8]"></span>  
         </p>
-        <router-link to="">
-          <p class="text-[#718EBF] hover:border-b-4 border-[#718EBF] pb-2">
-            <b>Endereços</b>
-          </p>
-        </router-link>
+        <p class="text-[#718EBF]">
+          <b>Endereços</b>
+        </p>
       </div>
     </nav>
     <div class="registro grid grid-cols-5 gap-4">
@@ -25,23 +23,27 @@ background: linear-gradient(90deg, #03B1FF 0%, #97C4D8 100%);
       </div>
       <div class="flex flex-col col-span-2">
         <label>Nome completo</label>
-        <input type="email" id="email" placeholder="Ex: Eduardo Evaristo" class="block max-w-[36rem]"></input>
+        <input type="text" id="nomeCompleto" placeholder="Ex: Eduardo Evaristo" class="max-w-[36rem] campos"></input>
         <label>Apelido</label>
-        <input type="email" id="email" placeholder="Ex: edueevaristo" class="block max-w-[36rem]"></input>
+        <input type="text" id="apelido" placeholder="Ex: edueevaristo" class="max-w-[36rem] campos"></input>
         <label>E-mail</label>
-        <input type="email" id="email" placeholder="Ex: eduardo@gmail.com" class="block max-w-[36rem]"></input>
+        <input type="email" id="email" placeholder="Ex: eduardo@gmail.com" class="max-w-[36rem] campos"></input>
         <label>Data de Nascimento</label>
-        <input type="email" id="email" placeholder="Ex: 00/00/0000" class="block max-w-[36rem]"></input>
+        <input type="date" id="data" class="max-w-[36rem] campos"></input>
       </div>
       <div class="flex flex-col col-span-2">
         <label>CPF</label>
-        <input type="email" id="email" placeholder="Ex: eduardo@gmail.com" class="block max-w-[36rem]"></input>
+        <input type="text" id="email" placeholder="Ex: 000.000.000-00" class="max-w-[36rem] campos"></input>
         <label>Gênero</label>
-        <input type="email" id="email" placeholder="Ex: eduardo@gmail.com" class="block max-w-[36rem]"></input>
+        <select id="genero" class="max-w-[36rem] campos">
+          <option>Masculino</option>
+          <option>Feminino</option>
+          <option>Outro..</option>
+        </select>
         <label>Senha</label>
-        <input type="email" id="email" placeholder="Ex: eduardo@gmail.com" class="block max-w-[36rem]"></input>
+        <input type="password" id="senha" placeholder="Ex: **********" class="max-w-[36rem] campos"></input>
         <label>Telefone</label>
-        <input type="email" id="email" placeholder="Ex: eduardo@gmail.com" class="block max-w-[36rem]"></input>
+        <input type="text" id="telefone" placeholder="Ex: (00) 0000-0000" class="max-w-[36rem] campos"></input>
       </div>
     </div>
     <div class="text-center">
@@ -59,7 +61,7 @@ background: linear-gradient(90deg, #03B1FF 0%, #97C4D8 100%);
     margin: 20px;
     border-radius: 10px;
 }
-#email{
+.campos{
     border-radius: 5px;
     color: black;
     background-color: #F1F3F6;
