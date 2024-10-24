2b4e72604d102839b271cc6ee66e095d8faadfc9
DanielVieraCosta <dvieiradacosta777@gmail.com>
2024-10-23 21:05:56 -0300

    ajustar posição dos campos de endereço

--- src/components/AddressUser.vue
+++ src/components/AddressUser.vue
@@ -31,47 +31,47 @@ function adicionarEndereco() {
       </div>
     </nav>
     <form @submit.prevent="adicionarEndereco" class="registro grid grid-cols-2 gap-10">
-      <div class="flex flex-col col-span-1 gap-2 ml-32">
+      <div class="flex flex-col col-span-1 gap-2 max-w-[36rem] w-full mx-auto">
         <label>Nome da rua</label>
         <div class="flex">
-          <input type="text" name="rua" v-model="listaEndereco.rua" placeholder="Ex: Nove de Julho" class="max-w-[36rem] w-full campos" required></input>
+          <input type="text" name="rua" v-model="listaEndereco.rua" placeholder="Ex: Nove de Julho" class="w-full campos" required></input>
           <svg class="h-11 w-11 icons"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8" />  <line x1="13" y1="7" x2="13" y2="7.01" />  <line x1="17" y1="7" x2="17" y2="7.01" />  <line x1="17" y1="11" x2="17" y2="11.01" />  <line x1="17" y1="15" x2="17" y2="15.01" />
           </svg>
         </div>
         <label>Bairro</label>
         <div class="flex">
-          <input type="text" name="bairro" v-model="listaEndereco.bairro" placeholder="Ex: Altaneira" class="max-w-[36rem] w-full campos" required></input>
+          <input type="text" name="bairro" v-model="listaEndereco.bairro" placeholder="Ex: Altaneira" class="w-full campos" required></input>
           <svg class="h-11 w-11 icons"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8" />  <line x1="13" y1="7" x2="13" y2="7.01" />  <line x1="17" y1="7" x2="17" y2="7.01" />  <line x1="17" y1="11" x2="17" y2="11.01" />  <line x1="17" y1="15" x2="17" y2="15.01" />
           </svg>
         </div>
         <label>CEP</label>
         <div class="flex">
-          <input type="text" name="cep" v-model="listaEndereco.cep" placeholder="Ex: 00000-000" class="max-w-[36rem] w-full campos" required></input>
+          <input type="text" name="cep" v-model="listaEndereco.cep" placeholder="Ex: 00000-000" class="w-full campos" required></input>
           <svg class="h-11 w-11 icons"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="11" r="3" />  <path d="M17.657 16.657L13.414 20.9a1.998 1.998 0 0 1 -2.827 0l-4.244-4.243a8 8 0 1 1 11.314 0z" />
           </svg>
         </div>
       </div>
-      <div class="flex flex-col col-span-1 gap-2 mr-32">
+      <div class="flex flex-col col-span-1 gap-2 max-w-[36rem] w-full mx-auto">
         <label>Complemento</label>
         <div class="flex">
-          <input type="text" name="complemento" v-model="listaEndereco.complemento" placeholder="Ex: Atrás do Tauste" class="max-w-[36rem] w-full campos"></input>
+          <input type="text" name="complemento" v-model="listaEndereco.complemento" placeholder="Ex: Atrás do Tauste" class="w-full campos"></input>
           <svg class="h-11 w-11 icons"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="12" y1="5" x2="12" y2="19" />  <line x1="5" y1="12" x2="19" y2="12" />
           </svg>
         </div>
         <label>Cidade</label>
         <div class="flex">
-          <input type="text" name="cidade" v-model="listaEndereco.cidade" placeholder="Ex: Ubatuba" class="max-w-[36rem] w-full campos" required></input>
+          <input type="text" name="cidade" v-model="listaEndereco.cidade" placeholder="Ex: Ubatuba" class="w-full campos" required></input>
           <svg class="h-11 w-11 icons"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8" />  <line x1="13" y1="7" x2="13" y2="7.01" />  <line x1="17" y1="7" x2="17" y2="7.01" />  <line x1="17" y1="11" x2="17" y2="11.01" />  <line x1="17" y1="15" x2="17" y2="15.01" />
           </svg>
         </div>
         <label>Número da residência</label>
         <div class="flex">
-          <input type="text" name="numeroResidencia" v-model="listaEndereco.numeroResidencia" placeholder="Ex: 000" class="max-w-[36rem] w-full campos" required></input>
+          <input type="text" name="numeroResidencia" v-model="listaEndereco.numeroResidencia" placeholder="Ex: 000" class="w-full campos" required></input>
           <svg class="h-11 w-11 icons"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
           </svg>
@@ -99,7 +99,7 @@ function adicionarEndereco() {
       </div>
     </ul> -->
 
-    <div class="text-right mt-14">
+    <div class="text-right mt-14 mr-10">
         <router-link to="/registration"><button class="button"><b>Voltar a etapa anterior</b></button></router-link>
         <router-link to="/"><button class="button"><b>Salvar</b></button></router-link>
     </div>
