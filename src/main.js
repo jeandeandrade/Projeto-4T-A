6e64522a0efc2b9719099ac098d3597bb750860f
Eduardo <eduevaristo_dev@hotmail.com>
2024-11-22 20:19:22 -0300

    [feature/sm-projeto-ajustes] - Commit Feature | Adicionando ajustes

--- src/main.js
+++ src/main.js
@@ -7,8 +7,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { IMaskDirective } from 'vue-imask';
 const app = createApp(App)
 
+app.directive('imask', IMaskDirective);
 app.use(router)
+
 app.use(createPinia())
 app.mount('#app')
