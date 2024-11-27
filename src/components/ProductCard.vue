0541434f23467a49f9985498d4e00d112bb6d04e
Malagadixx <127902976+Malagadixx@users.noreply.github.com>
2024-11-27 10:12:43 -0300

    Criamos as listagem por categoria, ajustamos o modal, criamos o sku e algumas outras

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -4,36 +4,45 @@
       v-if="onSale"
       class="absolute top-2 left-2 bg-red-100 text-red-500 text-xs font-semibold py-1 px-2 rounded"
     >
-      Sale
+      Oferta
     </div>
     <div class="flex justify-center items-center mb-4">
       <img :src="image" :alt="title" class="h-40 w-auto object-contain" />
     </div>
-    <h3 class="text-sm font-semibold text-gray-800 mb-2">
-      {{ title }}
+    <h3
+      class="items-center justify-center text-center font-semibold text-gray-800 mb-2"
+    >
+      {{ mark }} {{ title }}
     </h3>
+    <p class="text-sm text-gray-500 mb-2">Categoria: {{ category }}</p>
     <div class="flex items-center space-x-2">
       <span v-if="oldPrice" class="text-sm line-through text-gray-400">
         ${{ oldPrice }}
       </span>
-      <span class="text-sm text-gray-500">From</span>
+      <span class="text-sm text-gray-500">Por Apenas:</span>
       <span class="text-sm font-bold text-red-500">${{ newPrice }}</span>
     </div>
     <button
       @click="openModal"
       class="w-full mt-4 py-2 px-4 bg-gray-800 text-white text-sm font-semibold rounded hover:bg-gray-700"
     >
-      See Details
+      Ver detalhes
     </button>
   </div>
 
-  <div v-if="modalVisible" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+  <div
+    v-if="modalVisible"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+  >
     <div class="modal-content bg-white rounded-lg w-11/12 md:w-2/5 p-5">
       <div class="flex justify-between">
         <h3 class="text-sm font-semibold text-gray-800 mb-2">
           {{ title }}
         </h3>
-        <div v-if="onSale" class="bg-red-100 text-red-500 text-xs font-semibold py-1 px-2 rounded">
+        <div
+          v-if="onSale"
+          class="bg-red-100 text-red-500 text-xs font-semibold py-1 px-2 rounded"
+        >
           Sale
         </div>
       </div>
@@ -45,30 +54,38 @@
         </div>
         <div class="col-span-2 mt-8">
           <div class="flex items-center space-x-2 text-sm text-gray-500">
-            <span>Mark:</span>
+            <span>Marca:</span>
             <span>{{ mark }}</span>
           </div>
           <div class="flex items-center space-x-2 text-sm text-gray-500">
-            <span>Category:</span>
+            <span>Categoria:</span>
             <span>{{ category }}</span>
           </div>
           <div class="flex items-center space-x-2">
-            <span class="text-sm text-gray-500">Price:</span>
+            <span class="text-sm text-gray-500">Preço:</span>
             <span v-if="oldPrice" class="text-sm line-through text-gray-400">
               ${{ oldPrice }}
             </span>
-            <span class="text-sm text-gray-500">From</span>
+            <span v-if="onSale" class="text-sm text-gray-500">Por Apenas</span>
             <span class="text-sm font-bold text-red-500">${{ newPrice }}</span>
           </div>
         </div>
       </div>
-      <span class="text-sm font-semibold text-gray-800">Descrição detalhada do produto:</span>
+      <span class="text-sm font-semibold text-gray-800"
+        >Descrição detalhada do produto:</span
+      >
       <p class="text-sm text-gray-500 indent-4 mt-2">{{ description }}</p>
       <div class="text-right mt-8">
-        <button @click="modalVisible = false" class="mr-4 py-2 px-4 bg-gray-800 text-white text-sm font-semibold rounded hover:bg-gray-700">
+        <button
+          @click="modalVisible = false"
+          class="mr-4 py-2 px-4 bg-gray-800 text-white text-sm font-semibold rounded hover:bg-gray-700"
+        >
           Adicionar ao carrinho
         </button>
-        <button @click="modalVisible = false" class="py-2 px-4 bg-gray-800 text-white text-sm font-semibold rounded hover:bg-gray-700">
+        <button
+          @click="modalVisible = false"
+          class="py-2 px-4 bg-gray-800 text-white text-sm font-semibold rounded hover:bg-gray-700"
+        >
           Fechar
         </button>
       </div>
@@ -113,7 +130,11 @@ export default {
     description: {
       type: String,
       required: true,
-    }
+    },
+    sku: {
+      type: Number,
+      required: true,
+    },
   },
   setup() {
     const modalVisible = ref(false);
--- src/components/ProductPage.vue
+++ src/components/ProductPage.vue
@@ -3,14 +3,20 @@
     class="bg-neutral-100 mt-16 m-10 rounded-2xl md:h-[316px] px-6 shadow"
   >
     <div class="flex gap-4 items-center mb-4">
-      <h2 class="text-lg font-bold">Shop By Categories</h2>
-      <a href="#" class="text-sm text-gray-600 hover:underline">View all</a>
+      <h2 class="text-lg font-bold">Comprar por categorias</h2>
+      <button
+        @click="resetFilter"
+        class="text-sm text-gray-600 hover:underline"
+      >
+        Ver todos os produtos
+      </button>
     </div>
     <div class="flex gap-4 justify-between items-center mt-8 text-center">
       <div
         v-for="(category, index) in categories"
         :key="index"
         class="flex flex-col items-center cursor-pointer hover:bg-gray-100 rounded-full p-2"
+        @click="filterProducts(category.name)"
       >
         <div
           class="p-16 flex items-center justify-center rounded-full bg-white shadow-md"
@@ -21,9 +27,10 @@
       </div>
     </div>
   </section>
+
   <section class="mt-20 m-16 p-2 bg-neutral-100 grid grid-cols-6 gap-4">
     <ProductCard
-      v-for="(product, index) in products"
+      v-for="(product, index) in filteredProducts"
       :key="index"
       :image="product.image"
       :title="product.title"
@@ -49,11 +56,10 @@ export default {
     return {
       categories: [
         { name: "Laptops & Tablets", icon: "src/assets/icons/laptop.jpg" },
-        { name: "Televisions", icon: "src/assets/icons/LED-TV.jpg" },
-        { name: "PCs & Accessories", icon: "src/assets/icons/telefone.jpg" },
+        { name: "Televisão", icon: "src/assets/icons/LED-TV.jpg" },
         { name: "Audio & Video", icon: "src/assets/icons/hometheater.jpg" },
         {
-          name: "Gaming & Accessories",
+          name: "Periféricos Gamer",
           icon: "src/assets/icons/computador.jpg",
         },
         { name: "Mobile Phones", icon: "src/assets/icons/telefone.jpg" },
@@ -61,66 +67,166 @@ export default {
       products: [
         {
           image: "src/assets/icons/laptop.jpg",
-          title: "(Product 1) Laptops & Tablets",
+          title: "Mac Book Pro",
           mark: "Apple",
           category: "Laptops & Tablets",
           oldPrice: 99.0,
           newPrice: 49.0,
           onSale: true,
-          description: "A high-performance laptop suitable for all your professional and personal needs.",
+          sku: "4145345346345",
+          description:
+            "A high-performance laptop suitable for all your professional and personal needs.",
         },
         {
           image: "src/assets/icons/LED-TV.jpg",
-          title: "(Product 2) Televisions",
+          title: "LED TV",
           mark: "Samsung",
-          category: "Televisions",
+          category: "Televisão",
           oldPrice: 99.0,
           newPrice: 49.0,
           onSale: true,
-          description: "Experience stunning visuals and vibrant colors with this cutting-edge LED TV.",
+          sku: "04237856789",
+          description:
+            "Experience stunning visuals and vibrant colors with this cutting-edge LED TV.",
         },
         {
           image: "src/assets/icons/telefone.jpg",
-          title: "(Product 3) PCs & Accessories",
+          title: "Accessories",
           mark: "Dell",
-          category: "PCs & Accessories",
-          oldPrice: 99.0,
-          newPrice: 49.0,
-          onSale: true,
-          description: "Essential PC accessories to enhance your productivity and comfort.",
-        },
-        {
-          image: "src/assets/icons/laptop.jpg",
-          title: "(Product 4) Laptops & Tablets",
-          mark: "HP",
-          category: "Laptops & Tablets",
-          oldPrice: 99.0,
-          newPrice: 49.0,
-          onSale: false,
-          description: "Reliable and affordable laptop for everyday tasks and entertainment.",
-        },
-        {
-          image: "src/assets/icons/LED-TV.jpg",
-          title: "(Product 5) Televisions",
-          mark: "LG",
-          category: "Televisions",
-          oldPrice: 99.0,
-          newPrice: 49.0,
-          onSale: false,
-          description: "A sleek and modern television designed for ultimate viewing experience.",
-        },
-        {
-          image: "src/assets/icons/telefone.jpg",
-          title: "(Product 6) Mobile Phones",
-          mark: "Apple",
           category: "Mobile Phones",
           oldPrice: 99.0,
           newPrice: 49.0,
+          onSale: true,
+          sku: "545673789",
+          description:
+            "Essential PC accessories to enhance your productivity and comfort.",
+        },
+        {
+          image: "src/assets/icons/laptop.jpg",
+          title: "Pavilion",
+          mark: "HP",
+          category: "Laptops & Tablets",
+          oldPrice: "",
+          newPrice: 49.0,
           onSale: false,
-          description: "Stay connected with this feature-packed and stylish mobile phone.",
+          sku: "96726587",
+          description:
+            "Reliable and affordable laptop for everyday tasks and entertainment.",
+        },
+        {
+          image: "src/assets/icons/LED-TV.jpg",
+          title: "OLED TV",
+          mark: "LG",
+          category: "Televisão",
+          oldPrice: "",
+          newPrice: 49.0,
+          onSale: false,
+          description:
+            "A sleek and modern television designed for ultimate viewing experience.",
+          sku: "134534678",
+        },
+        {
+          image: "src/assets/icons/telefone.jpg",
+          title: "iPhone 12",
+          mark: "Apple",
+          category: "Mobile Phones",
+          oldPrice: "",
+          newPrice: 49.0,
+          onSale: false,
+          sku: "32567267",
+          description:
+            "Stay connected with this feature-packed and stylish mobile phone.",
+        },
+        {
+          image: "src/assets/icons/hometheater.jpg",
+          title: "Soundbar Bar 1000",
+          mark: "JBL",
+          category: "Audio & Video",
+          oldPrice: "",
+          newPrice: 49.0,
+          onSale: false,
+          sku: "546756367",
+          description:
+            "Stay connected with this feature-packed and stylish mobile phone.",
+        },
+        {
+          image: "src/assets/icons/hometheater.jpg",
+          title: "Soundbar HW-Q990D",
+          mark: "Samsung",
+          category: "Audio & Video",
+          oldPrice: 99.0,
+          newPrice: 49.0,
+          onSale: true,
+          sku: "13294425",
+          description:
+            "Stay connected with this feature-packed and stylish mobile phone.",
+        },
+        {
+          image: "src/assets/icons/hometheater.jpg",
+          title: "Orpheus GS550",
+          mark: "Redragon",
+          category: "Audio & Video",
+          oldPrice: "",
+          newPrice: 49.0,
+          onSale: false,
+          sku: "13294425",
+
+          description:
+            "Stay connected with this feature-packed and stylish mobile phone.",
+        },
+        {
+          image: "src/assets/icons/computador.jpg",
+          title: "Microfone QuadCast",
+          mark: "HyperX",
+          category: "Periféricos Gamer",
+          oldPrice: 99.0,
+          newPrice: 49.0,
+          onSale: true,
+          sku: "53470105",
+          description:
+            "Stay connected with this feature-packed and stylish mobile phone.",
+        },
+        {
+          image: "src/assets/icons/computador.jpg",
+          title: "Webcam Full HD",
+          mark: "Logitech",
+          category: "Periféricos Gamer",
+          oldPrice: 99.0,
+          newPrice: 49.0,
+          sku: "01299421",
+          onSale: true,
+          description:
+            "Stay connected with this feature-packed and stylish mobile phone.",
+        },
+        {
+          image: "src/assets/icons/computador.jpg",
+          title: "PlayStation®VR2",
+          mark: "Sony",
+          category: "Periféricos Gamer",
+          newPrice: 49.0,
+          oldPrice: "",
+          sku: "01299421",
+          onSale: false,
+          description:
+            "Stay connected with this feature-packed and stylish mobile phone.",
         },
       ],
+      filteredProducts: [],
     };
+  },
+  methods: {
+    filterProducts(categoryName) {
+      this.filteredProducts = this.products.filter(
+        (product) => product.category === categoryName
+      );
+    },
+    // Resetar filtro para mostrar todos os produtos
+    resetFilter() {
+      this.filteredProducts = this.products;
+    },
+  },
+  mounted() {
+    this.filteredProducts = this.products;
   },
 };
 </script>
