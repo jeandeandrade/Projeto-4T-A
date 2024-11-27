7e5b4c63a8070c316289bdeb2ceb031b742f1647
Daniel Vieira <dvieiradacosta777@gmail.com>
2024-11-26 23:13:28 -0300

    adição de preço, title e imagem no modal

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -27,36 +27,35 @@
     </button>
   </div>
 
-  <div
-    v-if="modalVisible"
-    class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
-  >
-    <div class="modal-content bg-white rounded-lg w-11/12 md:w-1/4 p-5">
-      <div
-        v-if="onSale"
-        class="absolute bg-red-100 text-red-500 text-xs font-semibold py-1 px-2 rounded"
-      >
-        Sale
-      </div>
-      <div class="flex justify-center items-center mb-4">
-        <img :src="image" :alt="title" class="h-40 w-auto object-contain" />
-      </div>
+  <div v-if="modalVisible" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div class="modal-content bg-white rounded-lg w-11/12 md:w-2/5 p-5">
       <h3 class="text-sm font-semibold text-gray-800 mb-2">
         {{ title }}
       </h3>
-      <div class="flex items-center space-x-2">
-        <span v-if="oldPrice" class="text-sm line-through text-gray-400">
-          ${{ oldPrice }}
-        </span>
-        <span class="text-sm text-gray-500">From</span>
-        <span class="text-sm font-bold text-red-500">${{ newPrice }}</span>
+      <div class="grid grid-cols-3">
+        <div class="col-span-1">
+          <div class="flex justify-center items-center mb-4">
+            <img :src="image" :alt="title" class="h-40 w-auto object-contain" />
+          </div>
+        </div>
+        <div class="flex items-center space-x-2 col-span-2">
+          <span class="text-sm text-gray-500">Price:</span>
+          <span v-if="oldPrice" class="text-sm line-through text-gray-400">
+            ${{ oldPrice }}
+          </span>
+          <span class="text-sm text-gray-500">From</span>
+          <span class="text-sm font-bold text-red-500">${{ newPrice }}</span>
+        </div>
       </div>
-      <button
-        @click="modalVisible = false"
-        class="w-full mt-4 py-2 px-4 bg-gray-800 text-white text-sm font-semibold rounded hover:bg-gray-700"
-      >
-        Fechar
-      </button>
+      <span class="text-sm font-semibold text-gray-800 mb-2">Descrição detalhada do produto:</span>
+      <div class="text-right mt-8">
+        <button @click="modalVisible = false" class="mr-4 py-2 px-4 bg-gray-800 text-white text-sm font-semibold rounded hover:bg-gray-700">
+          Adicionar ao carrinho
+        </button>
+        <button @click="modalVisible = false" class="py-2 px-4 bg-gray-800 text-white text-sm font-semibold rounded hover:bg-gray-700">
+          Fechar
+        </button>
+      </div>
     </div>
   </div>
 </template>
